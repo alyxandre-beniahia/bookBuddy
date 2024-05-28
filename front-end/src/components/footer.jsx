9d15816f8da6e96c7c9db9618c9c0f98b4c17385
arthur-clerc <arthur.clerc@laplateforme.io>
2024-05-28 10:17:35 +0200

    remake footer in scss for conflict resolution

--- front-end/src/components/footer.jsx
+++ front-end/src/components/footer.jsx
@@ -3,9 +3,12 @@ import NavbarFooter from '../components/navbarFooter';
 function Footer(){
     return(
         <footer>
+            <div className="liens">
+                <NavbarFooter />
+            </div>
             <div className="space">
                 <div className="phrase2">
-                    <h1>Explore Our Gallery : See<br></br> Our Sunscreen In Action</h1>
+                    <h1>Explore Our Gallery : See Our Sunscreen In Action</h1>
                 </div>
                 <div className="phrase">
                     <label htmlFor="email"></label>
@@ -14,9 +17,6 @@ function Footer(){
                 <div className="phrase">
                     <button className="bouton">Back to the Top</button>
             </div>
-            </div>
-            <div className="liens">
-                <NavbarFooter />
             </div>
         </footer>
     );
