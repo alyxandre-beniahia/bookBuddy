53b419f1355b6bdc337116446978703a5def3165
elamine-insanakib <elamine.insa-nakib@laplateforme.io>
2024-05-29 17:07:44 +0200

    card and fetch in progress

--- front-end/src/pages/collections.jsx
+++ front-end/src/pages/collections.jsx
@@ -1,8 +1,38 @@
+import React from 'react';
+import Card from '../components/card.jsx'
+
 function Collections(){
+    //ICI fonction pour fetch les livres
+    async function getBooks() {
+        const data = await fetch ('http://localhost:5000/api/books');
+        const books = data.json();
+        console.log(books);
+    }
+    getBooks();
+
     return(
-      
-                <h1>Collections</h1>
-           
+        <main>
+            <h1 className="title">All Collections</h1>
+            <div className='container__card'>
+            <Card data="books" />
+            <Card />
+            <Card />
+            <Card />
+            </div>
+            <div className='container__card'>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            </div>
+            <div className='container__card'>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            </div>
+            
+        </main>
     );
 }
 
