9735bf8064e5d08eed9d3320a01cb60e60e0029d
arthur-clerc <arthur.clerc@laplateforme.io>
2024-05-30 11:07:16 +0200

    maj merge authentification

---
+++ routes/users 2.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const auth = require("../middleware/auth");
+
+// @route  POST api/users
+// @desc   Register user
+router.post("/addUser", userController.registerUser);
+
+// @route  POST api/users/login
+// @desc   Authenticate user & get token
+router.post("/connexion", userController.loginUser);
+
+// @route  GET api/users/:id
+// @desc   Get user by ID
+router.get("/:id", auth, userController.getUser);
+
+// @route  PUT api/users/:id
+// @desc   Update user password
+router.put("/:id", auth, userController.updatePassword);
+
+// @route  DELETE api/users/:id
+// @desc   Delete user
+router.delete("/:id", auth, userController.deleteUser);
+
+module.exports = router;
