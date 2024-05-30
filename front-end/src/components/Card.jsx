799a177d9250d1c0d4ae3f2af1af4dfa853e954e
alyxandre-beniahia <alyxandre.beniahia@laplateforme.io>
2024-05-30 14:42:27 +0200

    addec cards and collection

---
+++ front-end/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Card ({book, key}){
+    const {title, author, pages, category} = book;
+    return (
+        <div className="card" key={key}>
+            <div className="card-body">
+                <h5 className="card-title">Titre: {title}</h5>
+                <p className="card-text">Auteur: {author}</p>
+                <p className="card-text">Nb de pages: {pages}</p>
+                <p className="card-text">Catégorie: {category}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
